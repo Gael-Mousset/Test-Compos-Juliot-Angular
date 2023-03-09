f2826bd1ab80d239ddfcd03699d9bea62c8b6453
Gael Mousset <gaelmousset@outlook.fr>
2023-03-09 19:20:16 +0100

    (fix) modification

--- src/app/book.service.ts
+++ src/app/book.service.ts
@@ -1,24 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, forkJoin } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
-  private apiURL = 'https://anapioficeandfire.com/api/books';
+  private apiURL = 'https://anapioficeandfire.com/api';
 
   constructor(private http: HttpClient) { }
 
-  getBooks(): Observable<any> {
-    return this.http.get<any>(this.apiURL);
+  getBooks(){
+    return this.http.get(`${this.apiURL}/books`);
   }
 
-  getBookById(id: string): Observable<any> {
-    return this.http.get<any>(this.apiURL + '/' + id);
+  getBookById(id: string) {
+    return this.http.get(`${this.apiURL}/books/${id}`);
   }
-
-  getCharactersByBookId(bookId: string): Observable<any> {
-    return this.http.get<any>(this.apiURL + '/' + bookId + '/characters');
+  getCharactersByBookId(bookId: string) {
+    return this.http.get(`${this.apiURL}/books/${bookId}`).pipe(
+      map((book: any) => {
+        return book.characters.map((characterUrl: string) => {
+          return this.http.get(characterUrl);
+        });
+      }),
+      switchMap((characterRequests: Observable<any>[]) => {
+        return forkJoin(characterRequests);
+      })
+    );
   }
 }
