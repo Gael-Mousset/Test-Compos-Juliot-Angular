c5baeb06bf7773c7801dff0dd514107884202734
Gael Mousset <gaelmousset@outlook.fr>
2023-03-09 19:18:31 +0100

    (add) route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookDetailComponent } from './book-detail/book-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: BookListComponent },
+  { path: 'book/:id', component: BookDetailComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
